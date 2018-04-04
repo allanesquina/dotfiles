42b9ec4d15eb9a2f41125ec25454f843730faddf
Allan Esquina <loloudxur@gmail.com>
2018-04-04 14:05:47 -0300

    Choices

--- setup.sh
+++ setup.sh
@@ -10,6 +10,7 @@ myecho() {
 	echo -e "- ${1}"
 	echo -e "${CYAN}"
 }
+
 sayok() {
 	CYAN='\033[00;36m'
 	GREEN='\033[1;32m'
@@ -17,19 +18,15 @@ sayok() {
 	echo -e "-------------------------------------------OK"
 	echo -e "${CYAN}"
 }
-setup() {
-	# Local vars 
-	DOTFILES=$HOME/dotfiles
-	DOTREPO=https://github.com/allanesquina/dotfiles.git
 
-	myecho "Updating"
-	sudo apt-get update
-
+curlSetup() {
 	# CURL
 	myecho "CURL"
 	sudo apt install curl
 	sayok
+}
 
+gitSetup() {
 	# GIT
 	myecho "GIT"
 	sudo apt install git
@@ -39,62 +36,92 @@ setup() {
 	git config --global user.name "Allan Esquina"
 	git config --global user.email "loloudxur@gmail.com"
 	sayok
+}
+
+setup() {
+	# Local vars 
+	DOTFILES=$HOME/dotfiles
+	DOTREPO=https://github.com/allanesquina/dotfiles.git
+
+	myecho "Updating"
+	sudo apt-get update
 
 	# Verify if dotfiles folder exists
 	if [ "$DOTFILES" ]; then
-		myecho "Removing dotfiles"
-		rm -rf $DOTFILES
+		myecho "Dotfiles already downloaded"
+		sayok
+	else
+		myecho "Downloading dotfiles from github into ${DOTFILES}"
+		git clone $DOTREPO $DOTFILES 
 		sayok
 	fi
 
-	myecho "Downloading dotfiles from github into ${DOTFILES}"
-	git clone $DOTREPO $DOTFILES 
-	sayok
-
-	# Applications
+	# Start the menu
 	applications
-	docker
-	yarn
+}
 
-	#fonts
-	fonts
+setupAll() {
+	curlSetup
+	gitSetup
+	vimSetup
+	agSetup
+	xtermSetup
+	tmuxSetup
+	nodeSetup
+	dockerSetup
+	yarnSetup
+	fontSetup
+	keyboardSetup
+}
 
+keyboardSetup() {
 	# Keyboard configuration
 	myecho "Configuring Keyboard speed rate"
 	xset r rate 185 70
 	sayok
-
 }
 
-applications() {
+vimSetup() {
 	# Vim instalation
 	myecho "Vim"
 	sudo apt install vim
-	ln -s $DOTFILES/vim/vimrc $HOME/.vimrc
+	ln -sf $DOTFILES/vim/vimrc $HOME/.vimrc
 	sayok
+}
+
+nodeSetup() {
 	# Node Instalation
 	myecho "NodeJS By NVM"
 	curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 	nvm install 8
 	sayok
+}
+
+tmuxSetup() {
 	# Tmux instalation
 	myecho "Tmux"
 	sudo apt install tmux
-	ln -s $DOTFILES/tmux.conf $HOME/.tmux.conf
+	ln -sf $DOTFILES/tmux.conf $HOME/.tmux.conf
 	sayok
+}
+
+xtermSetup() {
 	# Xterm Config
 	myecho "Xterm"
 	sudo apt-get install xterm
-	ln -s $DOTFILES/Xresources.hybird $HOME/.Xdefaults
+	ln -sf $DOTFILES/Xresources.hybird $HOME/.Xdefaults
 	xrdb .Xdefaults
 	sayok
+}
+
+agSetup() {
 	# Ag search
 	myecho "Ag search"
 	sudo apt-get install silversearcher-ag 
 	sayok
 }
 
-docker() {
+dockerSetup() {
 	myecho "Docker"
 	sudo apt-get update
 	sudo apt-get install \
@@ -115,7 +142,7 @@ docker() {
 	sayok
 }
 
-yarn() {
+yarnSetup() {
 	myecho "Yarn"
 	curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -                                                                                                   
 	echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list                                                                       
@@ -124,14 +151,73 @@ yarn() {
 	sayok
 }
 
-fonts() {
-    myecho "Fonts"
-    sudo cp $DOTFILES/fonts/*.ttf /usr/share/fonts/TTF/
-    sudo cp $DOTFILES/fonts/*.otf /usr/share/fonts/OTF/
-    fc-cache
-    sayok
+fontSetup() {
+	myecho "Fonts"
+	sudo cp $DOTFILES/fonts/*.ttf /usr/share/fonts/TTF/
+	sudo cp $DOTFILES/fonts/*.otf /usr/share/fonts/OTF/
+	fc-cache
+	sayok
+}
+
+watchmanSetup() {
+	cd ~
+	git clone https://github.com/facebook/watchman.git
+	cd watchman/
+	git checkout v4.7.0
+	sudo apt-get install -y autoconf automake build-essential python-dev
+	./autogen.sh 
+	./configure 
+	make
+	sudo make install
+
+	watchman --version
+	echo 999999 | sudo tee -a /proc/sys/fs/inotify/max_user_watches  && echo 999999 | sudo tee -a  /proc/sys/fs/inotify/max_queued_events && echo 999999 | sudo tee  -a /proc/sys/fs/inotify/max_user_instances && watchman  shutdown-server
+}
+
+applications() {
+	options=(
+		"curl"
+		"git"
+		"vim"
+		"ag"
+		"xterm"
+		"tmux"
+		"node"
+		"docker"
+		"yarn"
+		"font"
+		"keyboard"
+		"watchman"
+		"Install All"
+	)
+
+	echo -e "Choose your application to install"
+	select opt in "${options[@]}" "Quit"; do 
+
+	  case "$REPLY" in
+		1 ) curlSetup;break;;
+		2 ) gitSetup;break;;
+		3 ) vimSetup;break;;
+		4 ) agSetup;break;;
+		5 ) xtermSetup;break;;
+		6 ) tmuxSetup;break;;
+		7 ) nodeSetup;break;;
+		8 ) dockerSetup;break;;
+		9 ) yarnSetup;break;;
+		10 ) fontSetup;break;;
+		11 ) keyboardSetup;break;;
+		12 ) watchmanSetup;break;;
+		13 ) setupAll;break;;
+
+		$(( ${#options[@]}+1 )) ) echo "Goodbye!"; break;;
+		*) echo "Invalid option.";continue;;
+	  esac
+	done
+
+	if [ "$REPLY" != $(( ${#options[@]}+1 )) ] ; then
+		applications
+	fi
 }
 
 # Start the setup
 setup
-

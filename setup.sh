9869ab635643229fcdfd114111db8d3a659932ad
Allan Esquina <loloudxur@gmail.com>
2018-04-04 16:32:11 -0300

    Added docker compose/vs code

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
-#/usr/bin/env bash
+#usr/bin/env bash
 myecho() {
-	#    .---------- constant part!
-	#    vvvv vvvv-- the code from above
 	YELLOW='\033[1;33m'
 	RED='\033[0;31m'
 	NC='\033[0m'
@@ -20,14 +18,12 @@ sayok() {
 }
 
 curlSetup() {
-	# CURL
 	myecho "CURL"
 	sudo apt install curl
 	sayok
 }
 
 gitSetup() {
-	# GIT
 	myecho "GIT"
 	sudo apt install git
 	sayok
@@ -48,7 +44,7 @@ setup() {
 
 	# Verify if dotfiles folder exists
 	if [ "$DOTFILES" ]; then
-		myecho "Dotfiles already downloaded"
+		myecho "Dotfiles is already downloaded"
 		sayok
 	else
 		myecho "Downloading dotfiles from github into ${DOTFILES}"
@@ -69,44 +65,46 @@ setupAll() {
 	tmuxSetup
 	nodeSetup
 	dockerSetup
+	dockerComposeSetup
 	yarnSetup
 	fontSetup
 	keyboardSetup
+	visualStudioCodeSetup
 }
 
+# Keyboard configuration
 keyboardSetup() {
-	# Keyboard configuration
 	myecho "Configuring Keyboard speed rate"
 	xset r rate 185 70
 	sayok
 }
 
+# Vim instalation
 vimSetup() {
-	# Vim instalation
 	myecho "Vim"
 	sudo apt install vim
 	ln -sf $DOTFILES/vim/vimrc $HOME/.vimrc
 	sayok
 }
 
+# Node Instalation
 nodeSetup() {
-	# Node Instalation
 	myecho "NodeJS By NVM"
 	curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
 	nvm install 8
 	sayok
 }
 
+# Tmux instalation
 tmuxSetup() {
-	# Tmux instalation
 	myecho "Tmux"
 	sudo apt install tmux
 	ln -sf $DOTFILES/tmux.conf $HOME/.tmux.conf
 	sayok
 }
 
+# Xterm Config
 xtermSetup() {
-	# Xterm Config
 	myecho "Xterm"
 	sudo apt-get install xterm
 	ln -sf $DOTFILES/Xresources.hybird $HOME/.Xdefaults
@@ -114,13 +112,23 @@ xtermSetup() {
 	sayok
 }
 
+# Ag search
 agSetup() {
-	# Ag search
 	myecho "Ag search"
 	sudo apt-get install silversearcher-ag 
 	sayok
 }
 
+# Docker Compose
+dockerComposeSetup() {
+	myecho "Docker Compose"
+	sudo curl -L https://github.com/docker/compose/releases/download/1.20.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+	sudo chmod +x /usr/local/bin/docker-compose
+	docker-compose --version
+	sayok
+}
+
+# Docker config
 dockerSetup() {
 	myecho "Docker"
 	sudo apt-get update
@@ -142,6 +150,18 @@ dockerSetup() {
 	sayok
 }
 
+# Visual Studio Code
+visualStudioCodeSetup() {
+	myecho "Visual Studio Code"
+	TEMP_DEB="$(mktemp)"
+	wget -O "$TEMP_DEB" 'https://az764295.vo.msecnd.net/stable/79b44aa704ce542d8ca4a3cc44cfca566e7720f1/code_1.21.1-1521038896_amd64.deb'
+	sudo dpkg -i "$TEMP_DEB"
+	sudo apt-get install -f
+	rm -f "$TEMP_DEB"
+	sayok
+}
+
+# Yarn setup
 yarnSetup() {
 	myecho "Yarn"
 	curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -                                                                                                   
@@ -151,6 +171,7 @@ yarnSetup() {
 	sayok
 }
 
+# Fonts setup
 fontSetup() {
 	myecho "Fonts"
 	sudo cp $DOTFILES/fonts/*.ttf /usr/share/fonts/TTF/
@@ -159,7 +180,9 @@ fontSetup() {
 	sayok
 }
 
+# Watchman setup
 watchmanSetup() {
+	myecho "Watchman"
 	cd ~
 	git clone https://github.com/facebook/watchman.git
 	cd watchman/
@@ -172,27 +195,29 @@ watchmanSetup() {
 
 	watchman --version
 	echo 999999 | sudo tee -a /proc/sys/fs/inotify/max_user_watches  && echo 999999 | sudo tee -a  /proc/sys/fs/inotify/max_queued_events && echo 999999 | sudo tee  -a /proc/sys/fs/inotify/max_user_instances && watchman  shutdown-server
+	sayok
 }
 
 applications() {
 	options=(
 		"curl"
-		"git"
-		"vim"
-		"ag"
-		"xterm"
-		"tmux"
-		"node"
-		"docker"
-		"yarn"
-		"font"
-		"keyboard"
-		"watchman"
-		"Install All"
+		"Git"
+		"Vim"
+		"Ag"
+		"Xterm"
+		"Tmux"
+		"Node"
+		"Docker"
+		"Yarn"
+		"Font"
+		"Keyboard"
+		"Watchman"
+		"Docker Compose"
+		"Vs Code"
 	)
 
 	echo -e "Choose your application to install"
-	select opt in "${options[@]}" "Quit"; do 
+	select opt in "${options[@]}" "Install All" "Quit"; do 
 
 	  case "$REPLY" in
 		1 ) curlSetup;break;;
@@ -207,14 +232,16 @@ applications() {
 		10 ) fontSetup;break;;
 		11 ) keyboardSetup;break;;
 		12 ) watchmanSetup;break;;
-		13 ) setupAll;break;;
+		13 ) dockerComposeSetup;break;;
+		14 ) visualStudioCodeSetup;break;;
 
-		$(( ${#options[@]}+1 )) ) echo "Goodbye!"; break;;
+		$(( ${#options[@]}+1 )) ) setupAll;break;;
+		$(( ${#options[@]}+2 )) ) echo "Goodbye!"; break;;
 		*) echo "Invalid option.";continue;;
 	  esac
 	done
 
-	if [ "$REPLY" != $(( ${#options[@]}+1 )) ] ; then
+	if [ "$REPLY" != $(( ${#options[@]}+2 )) ] ; then
 		applications
 	fi
 }
